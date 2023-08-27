7cee5ed03852967719484447fb09d979a94f1926
alpererdin <alpererdin636@hotmail.com>
2023-08-28 01:15:31 +0300

    Level System Finalized

---
+++ Assets/Scripts/Signals/CoreGameSignals.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Signals
+{
+    public class CoreGameSignals : MonoBehaviour
+    {
+        #region Singleton
+
+        public static CoreGameSignals Instance;
+        
+        private void Awake()
+        {
+            if (Instance != this && Instance !=null)
+            {
+                Destroy(this);
+                return;
+            }
+
+            Instance = this;
+        }
+  
+
+        #endregion
+        
+        public UnityAction<byte> onLevelInitialize=delegate {  };
+        public UnityAction onClearActiveLevel=delegate {  };
+        public UnityAction onNextLevel=delegate {  };
+        public UnityAction onRestartLevel=delegate {  };
+        public UnityAction onReset=delegate {  };
+        
+        public Func<byte>onGetLevelValue = delegate { return 0;};
+    }
+}
